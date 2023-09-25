eada56bfe3cdc2fd6b178610236938134df27ddb
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-25 20:35:18 -0300

    Update BaseDatos.py

--- app/BaseDatos.py
+++ app/BaseDatos.py
@@ -46,7 +46,7 @@ class DatabaseConnection:
             cls._connection = None
 
     @classmethod
-    def insert_data(cls, query, params=None):
+    def insert_data(cls, query, params):
         conn = cls.get_connection()
         cursor = conn.cursor()
         try:
@@ -81,6 +81,22 @@ class DatabaseConnection:
     # query = "DELETE FROM tabla WHERE columna = %s"
     # params = ("valor_a_eliminar",)
     # DatabaseConnection.delete_data(query, params)
+
+    @classmethod
+    def obtener_ultimoservidor(cls):
+        try:
+            conn = cls.get_connection()
+            cursor = conn.cursor()
+            cursor.execute("SELECT serverID FROM servidor ORDER BY serverID DESC LIMIT 1")
+            last_server = cursor.fetchone()
+            if last_server:
+                return last_server[0]
+            else:
+                return None
+        except Exception as e:
+            print("Error al obtener el último servidor:", e)
+            return None
+
 
     @classmethod
     def create_if_not_exists(cls):
@@ -127,65 +143,70 @@ class DatabaseConnection:
             ] 
 
             create_table_servidor = """CREATE TABLE IF NOT EXISTS servidor (
-                serverID INT PRIMARY KEY,
+                serverID INT AUTO_INCREMENT PRIMARY KEY,
                 cantUser INT,
                 nombre VARCHAR(255) NOT NULL
             )"""
 
             # Agrega aquí los servidores predeterminados que deseas insertar
             default_servers = [
-                (1, 100, 'Literatura Fantastica'),
-                (2, 50, 'Locura Cinematografica'),
-                (3, 200, 'Relatos de Viaje'),
-                (4, 100, 'Salud y Bienestar'),
-                (5, 60, 'Central de Videojuegos'),
-                (6, 20, 'Fanaticos del Deporte'),
-                (7, 20, 'Caos Musical'),
+                (100, 'Literatura Fantastica'),
+                (50, 'Locura Cinematografica'),
+                (200, 'Relatos de Viaje'),
+                (100, 'Salud y Bienestar'),
+                (60, 'Central de Videojuegos'),
+                (20, 'Fanaticos del Deporte'),
+                (20, 'Caos Musical'),
             # Agrega más servidores predeterminados según sea necesario
             ]
-            create_table_rol = """CREATE TABLE IF NOT EXISTS rol (
-                rolID INT AUTO_INCREMENT PRIMARY KEY,
-                nombre VARCHAR(255) NOT NULL,
-                descripcion VARCHAR(255) NOT NULL,
-                fecha_asignacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )"""
+
             create_table_miembroServidor="""CREATE TABLE IF NOT EXISTS miembroServidor (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 usuario_id INT,
                 servidor_id INT,
-                rolID INT,
+                rol VARCHAR(60) NOT NULL,
                 fecha_unirse TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (rolID) REFERENCES rol (rolID),
                 FOREIGN KEY (usuario_id) REFERENCES usuario (userID),
                 FOREIGN KEY (servidor_id) REFERENCES servidor (serverID)
             )"""
-            create_table_canal = """CREATE TABLE IF NOT EXISTS canal (
+            create_table_canales = """CREATE TABLE IF NOT EXISTS canales (
                 canalID INT AUTO_INCREMENT PRIMARY KEY,
-                serverID INT,
+                serverID INT NOT NULL,
                 nombre VARCHAR(255) NOT NULL,
                 FOREIGN KEY (serverID) REFERENCES servidor (serverID)
             )"""
+            default_canal1= [
+                (1, 'Club de Lectura'),
+                (1, 'Escritores emergentes'),
+                (1, 'Recomendaciones'),
+            ]
+            default_canal2 = [
+                (2, 'Salud Mental y Felicidad'),
+                (2, 'Cine en Fantasía'),
+            ]
             create_table_mensaje ="""CREATE TABLE IF NOT EXISTS mensaje(
                 mensajeID INT AUTO_INCREMENT PRIMARY KEY,
                 canalID INT,
                 userID INT,
                 contenido VARCHAR(500) NOT NULL,
-                fecha DATETIME,
-                FOREIGN KEY (canalID) REFERENCES canal (canalID),
+                fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (canalID) REFERENCES canales (canalID),
                 FOREIGN KEY (userID) REFERENCES usuario (userID)
             )"""
             cur.execute(create_table_usuario)
             cur.execute(create_table_servidor)
-            cur.execute(create_table_rol)
             cur.execute(create_table_miembroServidor)
-            cur.execute(create_table_canal)
+            cur.execute(create_table_canales)
             cur.execute(create_table_mensaje)
             # Inserta los servidores predeterminados en la tabla servidor
-            insert_default_servers = "INSERT INTO servidor (serverID, cantUser, nombre) VALUES (%s, %s, %s)"
+            insert_default_servers = "INSERT INTO servidor (cantUser, nombre) VALUES (%s, %s)"
             insert_default_usuario = "INSERT INTO usuario (nombreusuario, nombre,apellido, correo,contraseña,fecha_nacimiento) VALUES (%s, %s, %s,%s,%s,%s)"
-            
+            insert_default_canal1 = "INSERT INTO canales (serverID,nombre) VALUES (%s, %s)"
+            insert_default_canal2 = "INSERT INTO canales (serverID,nombre) VALUES (%s, %s)"
             cur.executemany(insert_default_servers, default_servers)
             cur.executemany(insert_default_usuario, default_usuario)
+            cur.executemany(insert_default_canal1, default_canal1)
+            cur.executemany(insert_default_canal2, default_canal2)
             conn.commit()
             print("Base de datos y tablas creadas exitosamente.")
         finally:
